f9ac6ae11d69a40aa572f3d820fd7d3e5b9a12ad
azdine06 <oazdine06@gmail.com>
2026-02-24 10:35:17 +0000

    Exercises XP

---
+++ week_2/day_3/Exercises_XP.js
@@ -0,0 +1,91 @@
+// Exercice 1
+const person = {
+    name: 'John Doe',
+    age: 25,
+    location: {
+        country: 'Canada',
+        city: 'Vancouver',
+        coordinates: [49.2827, -123.1207]
+    }
+}
+
+const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+
+console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+// I am John Doe from Vancouver, Canada. Latitude(49.2827), Longitude(-123.1207)
+
+
+// Exercice 2
+function displayStudentInfo(objUser){
+    const {first,last}=objUser
+    console.log("Your full Name is "+first+" "+last)
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
+
+// Exercice 3
+const users = { user1: 18273, user2: 92833, user3: 90315 };
+
+
+const usersArray = Object.entries(users);
+
+
+const modifiedUsers = usersArray.map(([key, id]) => {
+    return [key, id * 2];
+});
+
+
+
+//Exercice 4
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const member = new Person('John');
+  console.log(typeof member);
+//OutPut ==> object
+
+// Exercice 5
+//this is the right answer 
+  // 2
+  class Labrador extends Dog {
+    constructor(name, size) {
+      super(name);
+      this.size = size;
+    }
+  };
+  
+//Exercice 6
+
+[2] === [2] 
+//false
+{} === {}
+//false
+
+const object1 = { number: 5 }; 
+const object2 = object1; 
+const object3 = object2; 
+const object4 = { number: 5};
+
+object1.number = 4;
+console.log(object2.number)//4 passed by referance
+console.log(object3.number)//4 passed by referance
+console.log(object4.number)//5 not passed by reference
+
+class Animal{
+    constructor(name,type,color){
+        this.name=name
+        this.type=type
+        this.color=color
+    }
+}
+
+class Mammal extends Animal{
+    sound(sound) {
+        return `${sound} I'm a ${this.type}, named ${this.name} and I'm ${this.color}`
+    }
+}
+const farmerCow= new Mammal("Lily","cow","black and white")
+farmerCow.sound("Moo")
